54c298111159237066a3d0186f645d8991464d8b
PhamTrung1234 <trungphamvantrung123@gmail.com>
2024-03-15 01:21:13 +0700

    sửa

--- QLproduct/JS/danhsach.js
+++ QLproduct/JS/danhsach.js
@@ -1,3 +1,25 @@
-function DSProduct(){
-    this.arrsp = [] 
+
+function sapxepxuong (arr){
+    for(let i= 0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(Number(arr[i].phoneprice)<Number(arr[j].phoneprice)){
+                let temp = arr[i];
+                arr[i]= arr[j];
+                arr[j]=temp
+            }
+        }
+    }
+    return arr
+}
+function sapxeplen (arr){
+    for(let i= 0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(Number(arr[i].phoneprice)>Number(arr[j].phoneprice)){
+                let temp = arr[i];
+                arr[i]= arr[j];
+                arr[j]=temp
+            }
+        }
+    }
+    return arr
 }
--- QLproduct/JS/main.js
+++ QLproduct/JS/main.js
@@ -1,75 +1,163 @@
 const api = new Api();
-let dssp = new DSProduct();
+let dssp = [];
 
-function getId(id){
-    return document.getElementById(id)
+function getId(id) {
+  return document.getElementById(id);
 }
 getlocal();
 
-
-function renderUI(data){
-    let content = ""
-    for(let i=0;i<data.length;i++){
-        let sp = data[i];
-        content += `
+function renderUI(data) {
+  let content = "";
+  for (let i = 0; i < data.length; i++) {
+    let sp = data[i];
+    content += `
         <tr>
-        <td>${i}</td>
+        <td>${i + 1}</td>
         <td>${sp.phonename}</td>
         <td>${sp.phoneprice}</td>
-        <td><img width="50" src="../img/${sp.image}" alt="..."></td>
+        <td><img width="50" src="./img/${sp.image}" alt="..."></td>
         <td>${sp.desc}</td>
         <td>
-        <button class="btn btn-danger">Xóa</button>
-        <button class="btn btn-primary">Sửa</button>
+        <button onclick="xoaproduct(${
+          sp.id
+        })" class="btn btn-danger">Xóa</button>
+        <button onclick="editproduct(${
+          sp.id
+        })" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Sửa</button>
         </td>
         </tr>
-        `
-    }
-    getId("tblDanhSachSP").innerHTML = content;
+        `;
+  }
+  getId("tblDanhSachSP").innerHTML = content;
 }
 
-function setlocal(data){
-    const arrstring = data
-    const arrJson = JSON.stringify(arrstring)
-    localStorage.setItem("phone",arrJson)
+function setlocal(data) {
+  const arrstring = data;
+  const arrJson = JSON.stringify(arrstring);
+  localStorage.setItem("phone", arrJson);
 }
-function getlocal(){
-    if(!localStorage.getItem("phone")) return;
-    const arrJson =localStorage.getItem('phone');
-    const arrstring =JSON.parse(arrJson)
-    dssp =arrstring
-    renderUI(arrstring);
+function getlocal() {
+  if (!localStorage.getItem("phone")) return;
+  const arrJson = localStorage.getItem("phone");
+  const arrstring = JSON.parse(arrJson);
+  dssp = arrstring;
+  renderUI(arrstring);
 }
-
-getId('themsp').onclick = function(){
-    const name = getId('TenSP').value;
-    const price = getId('GiaSP').value;
-    const screen =getId('screen').value;
-    const back = getId('backcam').value;
-    const font = getId('fontcam').value;
-    const img = getId('HinhSP').value;
-    const desr = getId('MoTa').value;
-    const type = getId('inptype').value;
-    const sp = new Product("",name,price,screen,back,font,img,desr,type,"")
-    api.addphone(sp)
-      .then(function(win){
-        console.log(win.data)
-        setlocal(win.data)
-        document.getElementsByClassName('close')[0].click()
-        getlistphone();
-      })
+function xoaproduct(id) {
+  api.deletephone(id).then(() => {
+    getlistphone();
+    setlocal();
+  });
 }
+function editproduct(id) {
+  api.editphone(id).then((win) => {
+    document.querySelector(".modal-title").innerHTML = "Edit Product";
+    getId("suasp").style.display = "block";
+    getId("themsp").style.display = "none";
+    getId("TenSP").value = win.data.phonename;
+    getId("GiaSP").value = win.data.phoneprice;
+    getId("screen").value = win.data.screen;
+    getId("backcam").value = win.data.backcamera;
+    getId("fontcam").value = win.data.fontcamera;
+    getId("HinhSP").value = win.data.image;
+    getId("MoTa").value = win.data.desc;
+    getId("inptype").value = win.data.type;
+  });
 
+  document.querySelector(".modal-footer").innerHTML = `
+  <button id="suasp" onclick="updateproduct(${id})" class="btn btn-success">Cập Nhật</button>
+  `;
+}
+function updateproduct(id) {
+  const name = getId("TenSP").value;
+  const price = getId("GiaSP").value;
+  const screen = getId("screen").value;
+  const back = getId("backcam").value;
+  const font = getId("fontcam").value;
+  const img = getId("HinhSP").value;
+  const desr = getId("MoTa").value;
+  const type = getId("inptype").value;
+  const sp = new Product(
+    id,
+    name,
+    price,
+    screen,
+    back,
+    font,
+    img,
+    desr,
+    type,
+    ""
+  );
+  console.log(sp);
+  api.updatephone(sp).then((win) => {
+    getlistphone();
+    setlocal();
+    document.getElementsByClassName("close")[0].click();
+  });
+}
+getId("themsp").onclick = function () {
+  const name = getId("TenSP").value;
+  const price = getId("GiaSP").value;
+  const screen = getId("screen").value;
+  const back = getId("backcam").value;
+  const font = getId("fontcam").value;
+  const img = getId("HinhSP").value;
+  const desr = getId("MoTa").value;
+  const type = getId("inptype").value;
+  const sp = new Product(
+    "",
+    name,
+    price,
+    screen,
+    back,
+    font,
+    img,
+    desr,
+    type,
+    ""
+  );
+  api.addphone(sp).then(function (win) {
+    console.log(win.data);
+    setlocal(win.data);
+    document.getElementsByClassName("close")[0].click();
+    getlistphone();
+  });
+};
 
+function getlistphone() {
+  api
+    .fesdata()
+    .then(function (win) {
+      dssp = win.data;
 
-function getlistphone(){
-    api.fesdata()
-      .then(function(win){
-        dssp = win.data
-       
-        renderUI(win.data)
-        setlocal(win.data)
-      })
-      .catch()
+      renderUI(win.data);
+      setlocal(win.data);
+    })
+    .catch();
 }
 getlistphone();
+getId("btnThemSP").onclick = () => {
+  document.querySelector(".modal-title").innerHTML = "Add Product";
+  getId("themsp").style.display = "block";
+  document.querySelector(".modal-footer").innerHTML = "";
+  getId("TenSP").value = "";
+  getId("GiaSP").value = "";
+  getId("screen").value = "";
+  getId("backcam").value = "";
+  getId("fontcam").value = "";
+  getId("HinhSP").value = "";
+  getId("MoTa").value = "";
+  getId("inptype").value = "";
+};
+console.log(sapxepxuong(dssp))
+
+function mychange(){
+  let sapxep = getId("sapxep1").value;
+  if(sapxep === "uptodown"){
+       renderUI(sapxepxuong(dssp))
+  }else if(sapxep==="downtoup"){
+       renderUI(sapxeplen(dssp))
+  }
+
+}
